300ac60c1f252aff691f84f47e991775f1da52ab
sudhir <sbhosale369@gmail.com>
2022-06-27 19:53:41 +0530

    changes in demo.py file.

--- Demo.py
+++ Demo.py
@@ -3,8 +3,11 @@ from log import logger
 try:
     a=int(input("Please enter the first value:"))
     b=int(input("Please enter the second value:"))
-    c=a%b
+    c=a+b
+    # d=a % b
+    logger.info("__Program Excuted Successfully__,and outpot will be {}".format(c))
     # logger.info("Output:{}",format(str(c)))
 except OSError as e:
-    # print("Error: %s : %s" %(c,e.strerror))
-    logger.info(f"Exception Raised:{str(e)}")
+    print("Error: %s : %s" %(c,e.strerror))
+    # logger.info(f"Exception Raised:{str(e)}")
+    logger.error("Exception Raised :{}".format(str(e)))
